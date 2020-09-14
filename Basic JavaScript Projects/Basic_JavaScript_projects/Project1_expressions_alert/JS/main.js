c082568e6329dfc519abfbdd5711f793f449d483
Musterjedi <69767172+Musterjedi@users.noreply.github.com>
2020-09-14 12:19:06 -0700

    Project1_expressions_alert

---
+++ Basic JavaScript Projects/Basic_JavaScript_projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,28 @@
+window.alert("hello");
+
+var A = "this is a string";
+var B = "Nope...what it really says.... is Hello big world";
+
+document.write("\"Hello my name is Joe and I like to ride my bike," +
+    " sometimes I like to grab my board and go to the Beach.\" But,");
+document.write();
+document.write(" only on a sunny day,\otherwise, I'll just stay home\<br>\and study.");
+
+var B = " Joe " + "Hood"
+document.write()
+var Family = " The Hoods",
+    Me = " Joe",
+    her = " Missy",
+    baby = " Beans is our Cat.";
+document.write(baby);
+var blue = " My bike is blue.";
+var blue = blue.fontcolor("blue");
+document.write(blue);
+document.write(3 * 3);
+
+function My_First_Function() { //defining a function and nameing isolation
+    var str = "This is not blue!"; //defining a variable and gving it a string value
+    var result = str.fontcolor("green"); //using the fontcolor method on str variable
+    document.getElementById("Green_Text").innerHTML = result; //putting the value of the result into HTML element with "Green_Text"id
+
+}
